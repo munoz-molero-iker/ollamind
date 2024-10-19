695dd2af171e5f60a5e448d91e6e3404e523c6ba
Panik <munoz.molero.iker@gmail.com>
2024-10-19 19:07:15 +0200

    Simple arguments parser

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::env;
+use std::process;
+
+fn main() {
+    
+    //? [Simple arguments parser]
+    let args: Vec<String> = env::args().collect();
+
+    //* If no arguments are passed
+    if args.len() < 2 {
+
+        println!("Usage: ollamind <arguments>");
+        process::exit(1);
+
+    }
+
+    //* If at least one orgument is passed
+    match args.get(1).map(|s| s.as_str()) {
+
+        // Arguments
+        Some("create") => println!("Function to create a model with a model file:"),
+        Some("delete") => println!("Function to delete a model:"),
+        Some("list") => println!("Function to list all created model:"),
+        Some("run") => println!("Function to run a model:"),
+        Some("rag") => println!("Function to edit the RAG assigned to a model:"),
+
+        // Flags
+        Some("-h") | Some("--help") => println!("Function to show all the usable arguments:"),
+        Some("-v") | Some("--version") => println!("Function to show the downloaded version:"),
+
+        // Exhaustive checks
+        Some(other) => println!("Unknown argument: {}", other),
+        None => println!("No arguments passed")
+        
+    }
+
+}
