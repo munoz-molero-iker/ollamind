74c17e7b9b33933e7f4baca5e8b5235f2ee29046
Panik <munoz.molero.iker@gmail.com>
2024-10-21 19:34:36 +0200

    Create config folder on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 use std::env;
+use std::fs;
 use std::process;
+use std::path::PathBuf;
 use tokio;
 mod commands;
 
 #[tokio::main]
 async fn main() {
     
+    //? [Create the needed directories and files]
+    //* Create the data folder on Windows
+    #[cfg(target_os = "windows")]
+    {
+        let mut config_dir = PathBuf::from(env::var("APPDATA").unwrap());
+        config_dir.push(".ollamind");
+
+        fs::create_dir_all(&config_dir).unwrap_or_else(|e| {
+            println!("Error creating config directory: {}", e);
+            process::exit(1);
+        });
+    }
+
+    //* Create the data folder on Unix
+    #[cfg(target_family = "unix")]
+    { 
+        println!("OllaMind doesn't support linux yet");
+        process::exit(1);
+    }
+
+
     //? [Simple arguments parser]
     let args: Vec<String> = env::args().collect();
 
